da9b48f01d9704dd2d9872e02f18373ea6d9f482
Hazem Krimi <krimihazem1@gmail.com>
2020-10-16 14:06:32 +0100

    Updates

---
+++ precache-manifest.f79311784c235c5ae4657a5f9f5be103.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ee97169eccf53ad3c08c1aa573b78a5",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "4be56d8493f7a5fcc4c0",
+    "url": "/react-weather-app/static/css/main.04e8937e.chunk.css"
+  },
+  {
+    "revision": "81ae95357f903eeaddec",
+    "url": "/react-weather-app/static/js/2.db679ee1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-weather-app/static/js/2.db679ee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4be56d8493f7a5fcc4c0",
+    "url": "/react-weather-app/static/js/main.eb1e8dd7.chunk.js"
+  },
+  {
+    "revision": "aacb36d1c4af6b3eab1b",
+    "url": "/react-weather-app/static/js/runtime-main.d770b3fe.js"
+  },
+  {
+    "revision": "853a2eaa02ceb3d994c5fbd37e39eddb",
+    "url": "/react-weather-app/static/media/facebook.853a2eaa.svg"
+  },
+  {
+    "revision": "548c14de027a34714fb137b6a591f0fc",
+    "url": "/react-weather-app/static/media/github.548c14de.svg"
+  },
+  {
+    "revision": "e5289347c3de259559fcd065ca39dfb4",
+    "url": "/react-weather-app/static/media/linkedin.e5289347.svg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/react-weather-app/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/react-weather-app/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/react-weather-app/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/react-weather-app/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/react-weather-app/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
