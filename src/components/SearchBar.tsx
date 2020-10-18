c176d509518b78bb7c5f46dfac05f0649dcc01ea
Hazem Krimi <krimihazem1@gmail.com>
2020-10-18 19:25:25 +0100

    Update search bar styles

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
+import SearchIcon from '../assets/search.svg';
 
-const Input = styled.input`
+const Wrapper = styled.div`
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr auto;
+    justify-content: center;
+    align-items: center;
     border: 1px solid rgba(0, 0, 0, 0.15);
     border-radius: 5px;
-    padding: 0.25rem 0.5rem;
     justify-self: flex-end;
-    width: 100%;
+    padding: 0.2rem 0.5rem;
+`;
+
+const Input = styled.input`
+    border: none;
+    border-radius: 5px;
 
     &::placeholder {
         color: rgba(0, 0, 0, 0.5);
@@ -19,18 +29,25 @@ const SearchBar: React.FC = () => {
     const history = useHistory();
 
     const search = (event: React.KeyboardEvent<HTMLInputElement>) => {
-        if (event.key === 'Enter') {
+        if (event.key === 'Enter' && query !== '') {
             history.push(`/search/${query}`);
             setQuery('');
         }
     }
 
     return (
-        <Input
-            placeholder='Search'
-            onKeyPress={search}
-            onChange={event => setQuery(event.target.value)}
-        />
+        <Wrapper>
+            <Input
+                placeholder='Search'
+                value={query}
+                onKeyPress={search}
+                onChange={event => setQuery(event.target.value)}
+            />
+            <img
+                src={SearchIcon}
+                alt='Search icon'
+            />
+        </Wrapper>
     );
 }
 
