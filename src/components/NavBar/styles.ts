b20db5a08eedec2aae76b634c547f44c8f77d8f7
Hazem Krimi <krimihazem1@gmail.com>
2021-09-19 18:53:03 +0100

    Add logo

--- src/components/NavBar/styles.ts
+++ src/components/NavBar/styles.ts
@@ -6,8 +6,20 @@ export const Wrapper = styled.nav`
 	align-items: center;
 	padding: 0.5rem 0rem;
 
-	h1 {
-		cursor: pointer;
-		justify-self: flex-start;
+	.logo {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+
+		img {
+			width: 36px;
+			height: 36px;
+		}
+
+		h1 {
+			margin-left: 0.5rem;
+			cursor: pointer;
+			justify-self: flex-start;
+		}
 	}
 `;
