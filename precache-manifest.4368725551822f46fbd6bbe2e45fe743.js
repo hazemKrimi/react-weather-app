12626e14fcf04096197bc444b709257004c81bad
Hazem Krimi <krimihazem1@gmail.com>
2021-09-20 23:00:43 +0100

    Updates

---
+++ precache-manifest.4368725551822f46fbd6bbe2e45fe743.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa1a75a19717c3dcfe5bf1b847de130",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "3ae8df852b7c4d2c7c94",
+    "url": "/react-weather-app/static/css/main.4315ed01.chunk.css"
+  },
+  {
+    "revision": "ff61e991881d22b15129",
+    "url": "/react-weather-app/static/js/2.aab638c9.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/react-weather-app/static/js/2.aab638c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ae8df852b7c4d2c7c94",
+    "url": "/react-weather-app/static/js/main.e6aa2e91.chunk.js"
+  },
+  {
+    "revision": "aacb36d1c4af6b3eab1b",
+    "url": "/react-weather-app/static/js/runtime-main.d770b3fe.js"
+  },
+  {
+    "revision": "65c48b64f99407f5800f298fea27c56b",
+    "url": "/react-weather-app/static/media/github.65c48b64.svg"
+  },
+  {
+    "revision": "5cea32b1f9b274a4fe2d10ed43b7c1db",
+    "url": "/react-weather-app/static/media/left-arrow.5cea32b1.svg"
+  },
+  {
+    "revision": "2e548c6ede977ebfcbbe1ba0c961dc15",
+    "url": "/react-weather-app/static/media/linkedin.2e548c6e.svg"
+  },
+  {
+    "revision": "75520b4a470b4916275654f3b1a15264",
+    "url": "/react-weather-app/static/media/logo.75520b4a.svg"
+  },
+  {
+    "revision": "845e73a46e444a53fdba4f17b315ca27",
+    "url": "/react-weather-app/static/media/right-arrow.845e73a4.svg"
+  },
+  {
+    "revision": "d198cd791ceb3a096e742c61ddef550a",
+    "url": "/react-weather-app/static/media/search.d198cd79.svg"
+  },
+  {
+    "revision": "ef7c26d5ab7567a869a3b55d8f58cad5",
+    "url": "/react-weather-app/static/media/twitter.ef7c26d5.svg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/react-weather-app/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/react-weather-app/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/react-weather-app/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/react-weather-app/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/react-weather-app/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
